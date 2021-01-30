5bdcbd510092be6c61bdb6fab969ac4867630463
Zein Kassem <zein.s.kassem@gmail.com>
2021-01-31 00:29:20 +0200

    task-12-fix

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,16 +1,15 @@
 export default function createReportObject(employeesList) {
-    
-    return {
-        
-        allEmployees: {...employeesList},
-        
-        getNumberOfDepartments(employeesList)
-        
-        {
-        
-            return Object.keys(employeesList).length;
-            
-        },
-      };
 
+  return {
+      
+    allEmployees: {...employeesList} , 
+
+    getNumberOfDepartments(employeesList)
+    
+    {
+    return Object.keys(employeesList).length;
+
+      },
+    };
 }
+
