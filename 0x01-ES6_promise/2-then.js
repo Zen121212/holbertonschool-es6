1c952cd34ea10b6101e82e1f1d1236e73a580b6c
Zein Kassem <zein.s.kassem@gmail.com>
2021-02-01 03:15:50 +0200

    task-2-fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,4 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' })).catch(() => Error()).finally(() => console.log('Got response from API'));
+  return promise.then(() => ({ status: 200, body: 'Success' })).catch(() => Error())
+    .finally(() => { console.log('Got response from API'); });
 }
