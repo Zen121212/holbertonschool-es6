ebbb0ee6c9c7ef6f0e6c8437b2656bfb1487cf1e
Zein Kassem <zein.s.kassem@gmail.com>
2021-02-03 22:38:39 +0200

    task-4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Curreny from './3-curreny';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  diplayFullPrice() {
+    return `${this.amount} ${this.currency.diplayFullCurrency()}`;
+  }
+
+  static conertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
